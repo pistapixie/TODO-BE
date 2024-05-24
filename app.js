2f571a23c81436380ea75bdcf91e918772670263
pistapixie <hleehslee@gmail.com>
2024-05-25 03:04:58 +0900

    [Feat] dotenv 패키지 설치 및 설정

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
 const cors = require('cors')
+require('dotenv').config()
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD
+console.log("mongouri",MONGODB_URI_PROD)
 
 app.use(bodyParser.json());
 app.use(cors())
