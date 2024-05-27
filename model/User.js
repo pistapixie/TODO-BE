effef48ef2816d2c8c126f75488237f5c9cf2653
pistapixie <hleehslee@gmail.com>
2024-05-27 11:25:30 +0900

    [Feat] 사용자 스키마에 토큰 생성 및 비밀번호 제거 메서드 추가
    - generateToken 메서드 추가: JWT 토큰을 생성하여 1시간 동안 유효하도록 설정
    - toJSON 메서드 추가: 응답 객체에서 비밀번호와 __v 필드를 제거하도록 설정

--- model/User.js
+++ model/User.js
@@ -1,26 +1,39 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
-const JWT_SECRET_KEY = process.
+require('dotenv').config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
-const userSchema = Schema({
-  name:{
-    type:String,
-    required:true
-  },
-  email:{
-    type:String,
-    required:true
-  },
-  password:{
-    type:String,
-    required:true
-  }
-},{timestamps:true});
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    },
+    { timestamps: true }
+);
 
-userSchema.methods.generateToken= function(){
-const token = jwt.sign({_id:this.id}, "shhhhh");
-}
-const User = mongoose.model('User',userSchema)
+// 토큰 생성
+userSchema.methods.generateToken = function () {
+    const token = jwt.sign({ _id: this._id }, JWT_SECRET_KEY, { expiresIn: '1h' });
+    return token;
+};
+
+// 비밀번호 제거
+userSchema.methods.toJSON = function () {
+    const obj = this._doc;
+    delete obj.password;
+    delete obj.__v;
+    return obj;
+};
+
+const User = mongoose.model('User', userSchema);
 module.exports = User;
